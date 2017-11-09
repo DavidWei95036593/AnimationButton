4880565f585143b7c438dd9046852dab0dc3e743
DavidWei95036593 <dw36593@pausd.us>
2017-11-09 09:32:55 -0800

    move button in to hte right

--- main.js
+++ main.js
@@ -12,3 +12,16 @@ let box1 = document.querySelector(".box1");
 button1.addEventListener("click",function(){
 	changeSize(box1);
 })
+function closeSize(element){
+	let X = element.className;
+	if(X.match("wide")){
+		element.className = X.replace(" wide","");
+
+	}else{
+		element.className +=" wide";
+	}
+}
+let X = document.querySelector("#close");
+X.addEventListener("click",function(){
+		closeSize(box1);
+})
